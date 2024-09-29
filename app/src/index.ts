70344311d8a2c648fe1bbf9b70722ef93879eb4a
kzkio114 <kzkio114@gmail.com>
2024-09-29 23:58:36 +0900

    編集

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono'
 
 const app = new Hono()
-app.get('/', (c) => c.text('honoをbunでdevサーバーを立ててます！dockerでやってます。'))
+app.get('/', (c) => c.text('Hello, World!'))
 
 export default {
   port: 3000,
