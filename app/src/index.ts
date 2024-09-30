500d5c90d880c6d970ca77d0d75d8d1ccb21f9aa
kzkio114 <kzkio114@gmail.com>
2024-09-30 23:20:32 +0900

    ボタンの追加

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,7 +1,26 @@
 import { Hono } from 'hono'
 
 const app = new Hono()
-app.get('/', (c) => c.text('Hello, World!'))
+
+// ボタンを含むHTMLを返す
+app.get('/', (c) => {
+  const html = `
+    <html>
+      <body>
+        <h1>Hello, Hono!</h1>
+        <form action="/clicked" method="POST">
+          <button type="submit">Click me!</button>
+        </form>
+      </body>
+    </html>
+  `
+  return c.html(html)
+})
+
+// ボタンがクリックされた時の処理
+app.post('/clicked', (c) => {
+  return c.text('Button was clicked!')
+})
 
 export default {
   port: 3000,
